fb1bdd472ab2466897162c7fdd8c835e3e7f658d
Sebastian Pielawa <aeqwaa@gmail.com>
2013-10-30 23:22:33 +0100

    Moved content of token::tokenizer to token removed the tokenizer module. Renamed get_tokens to tokenizer. Renamed aeq.rs to main.rs.

--- token.rs
+++ token.rs
@@ -1,5 +1,8 @@
 pub mod token {
 
+    use std::char::is_digit;
+    use std::from_str::from_str;
+
     pub struct Token {
         id: Tokens
     }
@@ -23,79 +26,86 @@ pub mod token {
         Open, Close
     }
 
-    pub mod tokenizer {
+    pub fn tokenizer(text: ~str) -> ~[Token] {
 
-        use super::*;
+        let mut tokens: ~[Token] = ~[];
+        
+        do iter(text) |ch, next| {
 
-        pub fn get_tokens(text: ~str) -> ~[Token] {
-            use std::char::is_digit;
+            let token = match ch {
+                '+' => { Token{ id: Operator(Add) } }
+                '-' => { Token{ id: Operator(Sub) } }
+                '*' => { Token{ id: Operator(Mul) } }
+                '/' => { Token{ id: Operator(Div) } }
+                '(' => { Token{ id: Parenthese(Open) } }
+                ')' => { Token{ id: Parenthese(Close) } }
 
-            let mut tokens: ~[Token] = ~[];
-            
-            do iter(text) |ch, next| {
-                let token = match ch {
-                    '+' => Token{ id: Operator(Add) },
-                    '-' => Token{ id: Operator(Sub) },
-                    '*' => Token{ id: Operator(Mul) },
-                    '/' => Token{ id: Operator(Div) },
-                    '(' => Token{ id: Parenthese(Open) },
-                    ')' => Token{ id: Parenthese(Close) },
-                    d if is_digit(d) => {
-                        match token_number(d, next, text) {
-                            Some(t) => t,
-                            None => Token{ id: Unknown(ch) }
-                        }
+                d if is_digit(d) => {
+                    match token_number(d, next, text) {
+                        Some(t) => { t }
+                        None    => { Token{ id: Unknown(ch) } }
                     }
-                    _ => Token{ id: Unknown(ch) }
-                }; 
-                tokens.push(token);
-            }
-
-            tokens
-        }
-
-        fn token_number(ch: char, next: &mut uint, text: &str) -> Option<Token> {
-            use std::char::is_digit;
-            use std::from_str::from_str;
-
-            let mut substr = ~"";
-            substr.push_char(ch);
-            loop {
-                if *next >= text.len() {
-                    break
                 }
-                let ch = text.char_range_at(*next).ch;
-                if is_digit(ch) || ch == '.' {
-                    substr.push_char(ch)
-                } else {
-                    break
-                }
-                *next =  text.char_range_at(*next).next;
-            }
-            let n = match from_str::<f64>(substr) {
-                Some(val) => val,
-                None => return None
-            };
 
-            Some(Token{ id: Constant(Number(n)) })
+                _   => { Token{ id: Unknown(ch) } }
+            }; 
+
+            tokens.push(token);
         }
 
-        fn iter(text: &str, op: &fn(c: char, next: &mut uint)) {
-            let mut i = 0u;
-            while i < text.len() {
-                let ch = text.char_range_at(i).ch;
-                let mut next = text.char_range_at(i).next;
-                op(ch, &mut next);
-                i = next
+        return tokens;
+    }
+
+    fn token_number(ch: char, next: &mut uint, text: &str) -> Option<Token> {
+
+        let mut substr = ~"";
+
+        substr.push_char(ch);
+
+        loop {
+            if *next >= text.len() {
+                break
             }
+
+            let ch = text.char_range_at(*next).ch;
+
+            if is_digit(ch) || ch == '.' {
+                substr.push_char(ch)
+            } else {
+                break
+            }
+
+            *next = text.char_range_at(*next).next;
         }
 
-        pub fn each(t: &[Token], op: &fn(t: &Token)) {
-            let mut n = 0u;
-            while n < t.len() {
-                op(&t[n]);
-                n += 1;
-            }
+        let n = match from_str::<f64>(substr) {
+            Some(n) => n,
+            None    => return None
+        };
+
+        return Some(Token{ id: Constant(Number(n)) });
+    }
+
+    fn iter(text: &str, op: &fn(c: char, next: &mut uint)) {
+
+        let mut n = 0u;
+
+        while n < text.len() {
+            let ch = text.char_range_at(n).ch;
+            let mut next = text.char_range_at(n).next;
+
+            op(ch, &mut next);
+            n = next
         }
-    } 
+    }
+
+    pub fn each(t: &[Token], op: &fn(t: &Token)) {
+
+        let mut n = 0u;
+
+        while n < t.len() {
+            op(&t[n]);
+            n += 1;
+        }
+    }
 }
